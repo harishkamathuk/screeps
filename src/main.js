a0595fb539ae4956c4e5a61ba743042bb81791c5
Harish Kamath <harishkamathuk@users.noreply.github.com>
2019-05-21 15:02:52 +0100

    Send a creep to harvest energy
    To send a creep to harvest energy, you need to use the methods described in the documentation section below. 
    
    Commands will be passed each game tick. The harvest method requires that the energy source is adjacent to the creep.
    
    You give orders to a creep by its name this way: Game.creeps['Harvester1']. Use the FIND_SOURCES constant as an argument to the Room.find method.
    
    Send your creep to harvest energy by typing code in the "Script" tab.
    Documentation:
    Game.creeps
    RoomObject.room
    Room.find
    Creep.moveTo
    Creep.harvest

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+// initialize creep on console
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+
+module.exports.loop = function () {
+    var creep = Game.creeps['Harvester1'];
+    var sources = creep.room.find(FIND_SOURCES);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+    }
+}
