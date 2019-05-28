edea45e83ee1f63eeda0679521594666b3f25ba8
Harish Kamath <harish.kamath@blackrock.com>
2019-05-28 17:51:56 +0100

    Re-factor the code to use spawnCreep correctly

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
+
     // check for memory entries of died creeps by iterating over Memory.creeps
     for (let name in Memory.creeps) {
         // and checking if the creep is still alive
@@ -35,35 +36,49 @@ module.exports.loop = function () {
     //  arrow function, which checks for the creep being a harvester
     var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
     var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
-    var name = undefined;
 
+    console.log("We need " + minimumNumberOfHarvesters + " and we have "
+        + numberOfHarvesters + " harvesters in service.");
+    console.log("We have " + numberOfUpgraders + " upgraders in service.");
 
-    console.log("We need " + minimumNumberOfHarvesters + " and we have " + numberOfHarvesters + " harvesters in service.")
-    console.log("We have " + numberOfUpgraders + " upgraders in service.")
+    // first check if we can spawn a new creep...
+    var newName = 'WorkerCheck';
+    var canSpawn = Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE],
+        newName, { dryRun: true });
 
-    // if not enough harvesters
-    if (numberOfHarvesters < minimumNumberOfHarvesters) {
-        // try to spawn one
-        // name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined,
-        //     { role: 'harvester', working: false});
-        console.log("Spawning a harvester now ... ")
-        name = Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE], undefined,
-            { role: 'harvester', working: false});
+    // ... if we can ...
+    if( canSpawn === OK) {
+
+        // ... generate a new custom suffix for the name of the new creep ...
+        newName = Game.time;
+
+        // if not enough harvesters
+        if (numberOfHarvesters < minimumNumberOfHarvesters) {
+            // try to spawn one
+            newName = 'Harvester' + newName;
+            console.log("Spawning a harvester: " + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,MOVE], newName,
+                { role: 'harvester', working: false});
+        }
+        else {
+            // else try to spawn an upgrader
+            // small change from what you saw in the video: for upgraders it makes
+            //  more sense to have two move parts because they have to travel further
+            newName = 'Harvester' + newName;
+            console.log("Spawning a upgrader: " + newName);
+            Game.spawns['Spawn1'].spawnCreep([MOVE, MOVE, CARRY, WORK], newName,
+                { role: 'upgrader', working: false});
+        }
+
+        if (Game.spawns["Spawn1"].spawning) {
+            var spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
+            Game.spawns["Spawn1"].room.visual.text(
+                "Spawning: " + spawningCreep.memory.role,
+                Game.spawns["Spawn1"].pos.x + 1,
+                Game.spawns["Spawn1"].pos.y,
+                { align: "left", opacity: 0.8 }
+            );
+        }
     }
-    else {
-        // else try to spawn an upgrader
-        // small change from what you saw in the video: for upgraders it makes
-        //  more sense to have two move parts because they have to travel further
-        // name = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined,
-        //     { role: 'upgrader', working: false});
-        console.log("Spawning a upgrader now ... ")
-        name = Game.spawns['Spawn1'].spawnCreep([MOVE, MOVE, CARRY, WORK], undefined,
-            { role: 'upgrader', working: false});
-    }
 
-    // print name to console if spawning was a success
-    // name > 0 would not work since string > 0 returns false
-    if (!(name < 0)) {
-        console.log("Spawned new creep: " + name);
-    }
 };
